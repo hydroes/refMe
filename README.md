ef264fda82c10682729c79b5584b1847fb05821f
Brian Gouws <brian.gouws@ottilus.com>
2015-02-19 17:01:57 +0000

    Main app logic

--- README.md
+++ README.md
@@ -2,3 +2,17 @@
 
 RefMe shopping cart test
 
+This should work out of the box.
+
+----------------------------------
+In a production environment the following steps need to be completed.
+
+Dependencies : npm & nodejs installed
+
+----------------------------------
+Installation steps:
+
+1) run npm install
+2) run bower install
+
+
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,101 @@
-angular.module('refMeApp', [])
-  .controller('ShoppingCartController', ['$scope', function($scope) {
-    $scope.todos = [
-      {text:'learn angular', done:true},
-      {text:'build an angular app', done:false}];
+'use strict';
 
-    $scope.addTodo = function() {
-      $scope.todos.push({text:$scope.todoText, done:false});
-      $scope.todoText = '';
+var app = angular.module('refMeApp', []);
+
+app.service('ShoppingCartService', [function() {
+
+}]);
+
+/**
+* In the real world I would write a test for this service!
+*/
+app.service('ShoppingCartService', [function() {
+    /**
+     * Private cart items
+     */
+    var _cartItems = [];
+
+    var addItem = function(item) {
+        _cartItems.push(item);
     };
+
+    var removeItem = function(item, key) {
+        _cartItems.splice(key, 1);
+    };
+
+    var removeAllItems = function() {
+        _cartItems = [];
+    };
+
+    var getItems = function() {
+        console.log('_cartItems', _cartItems)
+        return _cartItems;
+    };
+
+    var getCartTotal = function() {
+        var total = 0.0;
+
+        angular.forEach(_cartItems, function(item) {
+            total += item.cost * item.quantity;
+        });
+
+        return total;
+
+    };
+
+    /**
+     * Expose the functions that are needed for this service
+     */
+    return {
+       addItem:addItem,
+       getItems:getItems,
+       removeItem:removeItem,
+       removeAllItems:removeAllItems,
+       getCartTotal:getCartTotal
+    };
+}]);
+
+/**
+* Controller for this app
+*/
+app.controller('ShoppingCartController', ['$scope', 'ShoppingCartService', function($scope, ShoppingCartService) {
+    
+    /**
+     * List of available items that can be added to the cart
+     */
+    $scope.availableItems = [
+      {name:'Backend skills', cost:60.50, quantity: 1},
+      {name:'JS knowledge', cost:80.50, quantity: 1},
+      {name:'Front end skills', cost:60.00, quantity: 1}];
+
+    /**
+     * Get shopping cart items
+     */
+    $scope.shoppingCartItems = ShoppingCartService.getItems();
+
+    /**
+     * Add an item to the cart
+     */
+    $scope.addToCart = function(item, key) {
+        $scope.availableItems.splice(key, 1);
+        ShoppingCartService.addItem(item);
+        $scope.calculateTotal();
+    };
+
+    /**
+     * Remove an item from the cart
+     */
+    $scope.removeFromCart = function(item, key) {
+        ShoppingCartService.removeItem(item, key);
+        $scope.availableItems.push(item);
+        $scope.calculateTotal();
+    };
+
+    /**
+     * Calculate the cart total
+     */
+    $scope.calculateTotal = function() {
+        $scope.cartTotal = ShoppingCartService.getCartTotal();
+    };
+    
   }]);
